8733f27acf39873b424e7e59551deec0025e77a5
roman_krasevych <roman.krasevych@lezgro.com>
2015-02-16 14:04:28 +0200

    testing

--- e2e/projects/projects.po.js
+++ e2e/projects/projects.po.js
@@ -8,13 +8,13 @@ var ProjectsPage = function() {
   this.modal = element(by.css('.modal-project'));
   this.modalTitle = this.modal.element(by.css('h3.modal-title'));
   this.modalCloseBtn = this.modal.element(by.css('.btn-close'));
-  this.modalEditable = this.modal.element(by.css('.editable'));
+  this.modalEditable = this.modal.element(by.css('h3.modal-title .editable'));
   this.editTextField = this.modalEditable.element(by.css('.edit-field'));
   this.editBtnOK = this.modalEditable.element(by.css('button.text-success'));
   this.editBtnCancel = this.modalEditable.element(by.css('button.text-danger'));
   this.editInput = this.modalEditable.element(by.css('input'));
   this.modelInput = this.modalEditable.element(by.model('value'));
-  this.headerProject = this.modal.element(by.css('.edit-field'));
+  this.headerProject = this.modal.element(by.css('h3.modal-title .edit-field'));
   this.modalBackdrop = element(by.css('.modal-backdrop'))
 };
 
